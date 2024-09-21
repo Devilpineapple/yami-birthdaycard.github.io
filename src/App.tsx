62a0366f6170bb9518d699536ae95e4114030869
Luis Angel Macias de la Cruz <luisangel10018@gmail.com>
2024-09-21 14:14:26 +0200

    link to location

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
 // import logo from './logo.svg';
-import logo from './birthday_empty.png';
+import logo from './birthday_empty_v2.png';
 import './App.css';
+import { AddToCalendarButton } from 'add-to-calendar-button-react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-body">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          INSERTA TARJETA DE CUMPLEAÑOS AQUÍ
-          {/* Edit <code>src/App.tsx</code> and save to reload. */}
-        </p>
-        <a
-          className="App-link"
+        
+        <div className='App-div'>
+          <div className="div-link location" onClick={() => {
+            window.open('https://maps.app.goo.gl/do54pdBSCzMvUmSA7')
+          }}>
+
+          </div>
+          <div className='div-link date'>
+            
+          </div>
+          <div className='div-link menu'>
+          </div>
+        </div>
+        {/* <a */}
+          {/* className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        > */}
+          {/* Learn React */}
+        {/* </a> */}
+        <AddToCalendarButton
+              name="Test"
+              startDate="2024-10-19"
+              options={['Google']}
+              timeZone="Spain"
+              />
+      </div>
     </div>
   );
 }
