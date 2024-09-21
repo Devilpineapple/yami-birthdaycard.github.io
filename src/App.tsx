ad61afd1b7d0dea0bd4d24fa92e2b15c35d6d30d
Luis Angel Macias de la Cruz <luisangel10018@gmail.com>
2024-09-21 15:37:50 +0200

    Added jump to link

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,68 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import logo from './luz_logo.png';
 import location from './luz_location.png';
 import date from './luz_date.png';
 import menu from './luz_menu.png';
 import './App.css';
-// import { AddToCalendarButton } from 'add-to-calendar-button-react';
 
-function App() {
+enum PageState {
+  FrontPage,
+  FoodMenu
+}
+
+const App = () => {
+  const [loading, setLoading] = useState(false)
+  const [pageState, setPageState] = useState(PageState.FrontPage)
+
+  useEffect(() => {
+    if (loading) {
+      console.log('loading start')
+      setTimeout(() => {
+        console.log('loading finished')
+        setPageState(pageState === PageState.FrontPage ? PageState.FoodMenu : PageState.FrontPage)
+        setLoading(false)
+      }, 500) // 0.5s seconds
+    }
+  }, [loading])
+
+  const onLocationClick = () => {
+    window.open('https://maps.app.goo.gl/do54pdBSCzMvUmSA7')
+  }
+
+  const onDateClick = () => {
+    window.open('https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=MWNlYmhraTVlOG4yOWVqMW5zZGoxODA1Z2UgbHVpc2FuZ2VsMTAwMThAbQ&tmsrc=luisangel10018%40gmail.com')
+  }
+
+  const onFoodMenuClick = () => {
+    setLoading(true)
+  }
+
+  const renderFrontPage = () => {
+    return <>
+      <img src={logo} className="App-logo" alt="logo" />
+
+      <div className='app-menu'>
+        <img src={location} className='menu-options' alt="logo" onClick={onLocationClick} />
+        <img src={date} className='menu-options' alt="logo" onClick={onDateClick} />
+
+        <img src={menu} className='menu-options' alt="logo" onClick={onFoodMenuClick} />
+      </div>
+    </>
+  }
+
+  const renderFoodMenu = () => {
+    return <>
+      Menu goes here
+      <img src={menu} className='menu-options' alt="logo" onClick={onFoodMenuClick} />
+    </>
+  }
+
   return (
     <div className="App">
       <div className="App-body">
-        <img src={logo} className="App-logo" alt="logo" />
 
-        <div className='app-menu'>
-          <img src={location} className='menu-options' alt="logo" onClick={() => {
-            window.open('https://maps.app.goo.gl/do54pdBSCzMvUmSA7')
-          }} />
-          <img src={date} className='menu-options' alt="logo" />
-          
-          <img src={menu} className='menu-options' alt="logo" />
-        </div>
-        
-        {/* <div className='App-div'>
-          <div className="div-link location" onClick={() => {
-            window.open('https://maps.app.goo.gl/do54pdBSCzMvUmSA7')
-          }}>
+        {pageState === PageState.FrontPage ? renderFrontPage() : renderFoodMenu()}
 
-          </div>
-
-          <div className='div-link date'>
-            
-          </div>
-          <div className='div-link menu'>
-          </div>
-        </div> */}
-        {/* <a */}
-          {/* className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        > */}
-          {/* Learn React */}
-        {/* </a> */}
-        {/* <AddToCalendarButton
-              name="Test"
-              startDate="2024-10-19"
-              options={['Google']}
-              timeZone="Spain"
-              /> */}
       </div>
     </div>
   );
