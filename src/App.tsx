ea521177cb5b7a3e5555f9391df6511190bb0aaf
Luis Angel Macias de la Cruz <luisangel10018@gmail.com>
2024-09-21 14:43:17 +0200

    updated images and CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,37 @@
 import React from 'react';
 // import logo from './logo.svg';
-import logo from './birthday_empty_v2.png';
+import logo from './luz_logo.png';
+import location from './luz_location.png';
+import date from './luz_date.png';
+import menu from './luz_menu.png';
 import './App.css';
-import { AddToCalendarButton } from 'add-to-calendar-button-react';
+// import { AddToCalendarButton } from 'add-to-calendar-button-react';
 
 function App() {
   return (
     <div className="App">
       <div className="App-body">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <div className='app-menu'>
+          <img src={location} className='menu-options' alt="logo" />
+          <img src={date} className='menu-options' alt="logo" />
+          <img src={menu} className='menu-options' alt="logo" />
+        </div>
         
-        <div className='App-div'>
+        {/* <div className='App-div'>
           <div className="div-link location" onClick={() => {
             window.open('https://maps.app.goo.gl/do54pdBSCzMvUmSA7')
           }}>
 
           </div>
-          
+
           <div className='div-link date'>
             
           </div>
           <div className='div-link menu'>
           </div>
-        </div>
+        </div> */}
         {/* <a */}
           {/* className="App-link"
           href="https://reactjs.org"
@@ -31,12 +40,12 @@ function App() {
         > */}
           {/* Learn React */}
         {/* </a> */}
-        <AddToCalendarButton
+        {/* <AddToCalendarButton
               name="Test"
               startDate="2024-10-19"
               options={['Google']}
               timeZone="Spain"
-              />
+              /> */}
       </div>
     </div>
   );
